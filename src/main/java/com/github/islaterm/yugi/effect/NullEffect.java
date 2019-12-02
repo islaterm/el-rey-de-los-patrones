876e42b29cdf3dc8ee15a4e1dcbfa70a005a564a
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2019-12-02 17:00:38 -0300

    Ejercicio 9

--- src/main/java/com/github/islaterm/yugi/effect/NullEffect.java
+++ src/main/java/com/github/islaterm/yugi/effect/NullEffect.java
@@ -1,8 +1,20 @@
 package com.github.islaterm.yugi.effect;
 
 public class NullEffect implements IEffect {
+  private IEffect instance;
+
+  private NullEffect() {
+  }
+
   @Override
   public void use() {
     System.out.println("No hago nada :D");
+  }
+
+  public IEffect getInstance() {
+    if (instance == null) {
+      instance = new NullEffect();
+    }
+    return instance;
   }
 }
